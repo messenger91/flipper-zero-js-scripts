1b162cb73547e21760f81e3758dcb3d3c2f6e3ff
Dmitriy Doronin <dmitriy435@gmail.com>
2024-03-22 18:01:03 +0400

    sparkles: Add custom_scripts folder Add chushpan.js script

---
+++ custom_scripts/chushpan.js
@@ -0,0 +1,10 @@
+
+// chushpan.js
+delay(2000)
+print("Init search chushpan...");
+delay(2000)
+print("Check data...");
+delay(2000)
+print("Chushpan of the day");
+print("It's you :)");
+print('------------')
